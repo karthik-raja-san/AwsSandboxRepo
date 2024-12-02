ec2e7d80a4e63b01c788c7a20429f916c7ae663e
karthikraja_sangaiah <karthikraja_sangaiah@epam.com>
2024-12-02 16:09:37 +0530

    demo

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -1,19 +1,22 @@
 package com.task01;
+import java.util.HashMap;
+import com.syndicate.deployment.*;
+import java.util.Map;
 
-import java.util.*;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.syndicate.deployment.*;
+import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
 @LambdaHandler(lambdaName="cmtr-1e70b2bd-hello_world-test", roleName="hello_world-role", isPublishVersion= false )
-class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<Object, Map<String,Object>> {
 
-    @Override
-    public Map<String, Object> handleRequest(Object request, Context context) {
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("statusCode", 200);
-        resultMap.put("message", "Hello from Lambda")
-        return resultMap;
-    }
+	@Override
+	public Map<String,Object> handleRequest(Object input, Context context) {
+		 Map<String, Object> resultMap = new HashMap<>();
+		 resultMap.put("statusCode", 200);
+		 resultMap.put("message", "Hello from Lambda");
+		 return resultMap;
+	}
+
 }
